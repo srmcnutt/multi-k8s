b06101943219c97251bb49625d543fc25bfe6457
Steven McNutt <mcnutt@lcti.net>
2019-11-29 15:11:51 -0500

    added deployment files

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+docker build -t smcnutt/multi-client:lastest -t smcnutt/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t smcnutt/multi-server:latest -t smcnutt/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t smcnutt/multi-worker:latest -t smcnutt/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push smcnutt/multi-client:latest
+docker push smcnutt/multi-server:latest
+docker push smcnutt/multi-worker:latest
+docker push smcnutt/multi-client:$SHA
+docker push smcnutt/multi-server:$SHA
+docker push smcnutt/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=smcnutt/multi-server:$SHA
+kubectl set image deployments/server-deployment worker=smcnutt/multi-worker:$SHA
+kubectl set image deployments/server-deployment client=smcnutt/multi-client:$SHA
